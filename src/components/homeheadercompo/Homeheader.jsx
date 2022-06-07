1fe57b1ec932348e640c9e0dafdda570907b65e8
chue wathan kyaw <chuewathan.k24@gmail.com>
2022-06-07 15:21:37 +0630

    hssl

---
+++ src/components/homeheadercompo/Homeheader.jsx
@@ -0,0 +1,14 @@
+import "./homeheader.css";
+
+const HomeHeader = () => {
+  return (
+    <>
+      <section>
+        <div className="header">
+          <h2>Myanfobase</h2>
+        </div>
+      </section>
+    </>
+  );
+};
+export default HomeHeader;
