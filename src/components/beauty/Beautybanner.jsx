d52843593ea637210b15fcd0160b401bf431414a
chue wathan kyaw <chuewathan.k24@gmail.com>
2022-06-28 22:01:26 +0630

    vv

---
+++ src/components/beauty/Beautybanner.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./beautybanner.css";
+export default function Beautybanner () {
+  return (
+    <section className="beauty-banner container">
+      
+      <div className="beauty-firstrow">
+        <div className="firstrow-firstdiv">
+          <div className="blogger-img">
+            <img src="./images/homeimgs/May-Oo-Makeup-7.jpg" alt="" />
+          </div>
+          <div className="blogger-para">
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
+              iusto deserunt fuga nam soluta quae suscipit incidunt, consectetur
+              totam tenetur modi illo neque! Voluptas cupiditate reiciendis
+              consectetur? Unde, expedita saepe.
+            </p>
+            <span className="first-overlawyer"></span>
+          </div>
+        </div>
+        <div className="firstrow-seccol">
+          <div className="firstrow-secdiv">
+            <div className="skincare-img">
+              <img src="./images/homeimgs/skincare.jpg" alt="" />
+            </div>
+            <div className="skincare-para">
+              <h4>Lorem ipsum dolor sit amet.</h4>
+            </div>
+          </div>
+          <div className="firstrow-thirddiv">
+            <div className="makeup-img">
+              <img src="./images/homeimgs/makeup.jpg" alt="" />
+            </div>
+            <div className="makeup-para">
+              <h4>Lorem ipsum dolor sit amet.</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="ban-secrow">
+          <div className="secrow-firstdiv">
+            <div className="beauty-img">
+              <img src="./images/homeimgs/beauty-salon.jpg" alt="" />
+            </div>
+            <div className="beauty-para">
+              <h4>Lorem ipsum dolor sit amet.</h4>
+            </div>
+          </div>
+
+          <div className="secrow-secdiv">
+            <div className="clinic-img">
+              <img src="./images/homeimgs/skin-clinic.jpg" alt="" />
+            </div>
+            <div className="clinic-para">
+              <h4>Lorem ipsum dolor sit amet.</h4>
+            </div>
+          </div>
+          <div className="secrow-thirddiv">
+            <div className="natural-img">
+              <img src="./images/homeimgs/natural.jpg" alt="" />
+            </div>
+            <div className="natural-para">
+              <h4>Lorem ipsum dolor sit amet.</h4>
+            </div>
+          </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/pages/beauty/Beauty.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Beautybanner from "../../components/beauty/Beautybanner";
+import Beautyheader from "../../components/beauty/Beautyheader";
+
+
+export default function Beauty() {
+  return (
+    <div>
+      <Beautyheader />
+      <Beautybanner />
+    </div>
+  );
+}
