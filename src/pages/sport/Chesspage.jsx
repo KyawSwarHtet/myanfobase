642f5e056815df57a1368c1543c6cbff88a216ff
thawzinhlaine <thawzinhlinn2382002@gmail.com>
2022-07-11 14:11:30 +0630

    sfsldf

--- src/pages/sport/Chesspage.jsx
+++ src/pages/sport/Chesspage.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import Latestbar from '../../components/catepagerightbar/Latestbar'
-import Footer from '../../components/footer/Footer'
-import SportMain from '../../components/sport/SportMain'
-import SportPopular from '../../components/sport/SportPopular'
-import Chessbanner from '../../components/sport/sportsetmenu/chess/Chessbanner'
-import Chessheader from '../../components/sport/sportsetmenu/chess/Chessheader'
+import React from "react";
+import Latestbar from "../../components/catepagerightbar/Latestbar";
+import Footer from "../../components/footer/Footer";
+import SportMain from "../../components/sport/SportMain";
+import SportPopular from "../../components/sport/SportPopular";
+import Chessbanner from "../../components/sport/sportsetmenu/chess/Chessbanner";
+import Chessheader from "../../components/sport/sportsetmenu/chess/Chessheader";
 
 export default function Chesspage() {
   return (
     <>
-          <section className='container'>
-              <Chessheader/>
-          </section>
-          <Chessbanner />
-          <section className='container'>
-              <div className='sportbody'>
-                  <SportMain />
-                  <div className='allRightBar'>
-                      <SportPopular />
-                      <Latestbar/>
-                  </div>
-              </div>
-          </section>
-          <Footer/>
+      <section className="container">
+        <Chessheader />
+      </section>
+      <Chessbanner />
+      <section className="container">
+        <div className="sportbody">
+          <SportMain />
+          <div className="allRightBar">
+            <SportPopular />
+            <Latestbar />
+            <h1>sldflsdafklsdjf</h1>
+            <h1>sldflsdafklsdjf</h1>
+            <h1>sldflsdafklsdjf</h1>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </>
-  )
+  );
 }
