5bb9e3e1666fe851cf39e58274efa327da4fcc53
thawzinhlaine <thawzinhlinn2382002@gmail.com>
2022-07-12 10:53:37 +0630

    addd

--- src/pages/detailpage/DetailPage.jsx
+++ src/pages/detailpage/DetailPage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import Latestbar from "../../components/catepagerightbar/Latestbar";
+
 import Detailbanner from "../../components/detailpagecompo/Detailbanner";
+import Latestdetail from "../../components/detailpagecompo/Latestdetail";
+import Latestpho from "../../components/detailpagecompo/Latestpho";
+import RelatedA from "../../components/detailpagecompo/RelatedA";
 import Footer from "../../components/footer/Footer";
-import LiteraturePopular from "../../components/literature/LiteraturePopular";
+
 import "./detailpage.css";
 
 export default function DetailPage() {
@@ -16,8 +20,9 @@ export default function DetailPage() {
         <Detailbanner detailid={detailid} />
 
         <div className="allRightBar">
-          <LiteraturePopular />
-          <Latestbar />
+          <RelatedA />
+          <Latestpho/>
+          <Latestdetail/>
         </div>
       </section>
       <Footer />
