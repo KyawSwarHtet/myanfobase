c13f81a9abca73531079cd40e1fa0fc3dd9b1fd6
kyaw swar htet <kyawswarhtet101094@gmail.com>
2022-07-13 17:58:56 +0630

    postksh

--- src/components/post/Postform.jsx
+++ src/components/post/Postform.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { useState } from "react";
 import DropdownCate from "./DropdownCate";
+import FormSample from "./FormSample";
 import "./postform.css";
+import Textform from "./Textform";
 
 export default function Postform() {
   const [selected, setSelected] = useState("Choose One");
   return (
     <>
       <section className="postbg">
-        <div className="container">
+        <div className="container firstdesc">
           <h2 className="capitalize">
             Let's share your experience and knowledge on our website!
           </h2>
@@ -20,20 +22,19 @@ export default function Postform() {
           </p>
         </div>
       </section>
-      <div className="container">
+      <div className="container formBody">
         <DropdownCate selected={selected} setSelected={setSelected} />
         <div className="formDiv">
-          <form className="postForm">
-            <textarea
-              className="titleArea"
-              placeholder="Type title here..."
-            ></textarea>
+          <form
+            className="postForm"
+            action=""
+            method=""
+            enctype="multipart/form-data"
+          >
+            <Textform />
+            <FormSample minRows={1} />
 
-            <textarea
-              className="descArea"
-              placeholder="Description here..."
-              required
-            ></textarea>
+            <button className="postButton">Post</button>
           </form>
         </div>
       </div>
